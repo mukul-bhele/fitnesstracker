4c9b522260d711f4d0fe84b904d9bdc65fe6c651
mukul-bhele <145564456+mukul-bhele@users.noreply.github.com>
2023-10-05 11:09:58 +0530

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -18,8 +18,6 @@ CREATE TABLE Daily_Activity (
 	Day VARCHAR(50)
 );
 
-![image](https://github.com/mukul-bhele/fitnesstracker/blob/fd1f615b3aa77d3da602ff8ef8227593090b512d/Activity%20Metrics%20Analysis%20-%20Image.png)
-
 -- Creating Table 'Sleep'
 CREATE TABLE Sleep (
     Id VARCHAR(50),
@@ -40,18 +38,26 @@ CREATE TABLE Sleep (
 	Day VARCHAR(50)
 );
 
+-- Creating Table 'MET'
 CREATE TABLE MET (
 	Id VARCHAR(50),
     ActivityDate DATE,
     METs INT
 );
 
-
+-- Viewing data from each table
 SELECT * FROM Daily_Activity;
 SELECT * FROM Sleep;
 SELECT * FROM MET;
 
 -- Checking users insigths
+/* 
+    This SQL code is calculating the distribution of users based on the number of times they have used the fitness tracker.
+    It counts how many users fall into each frequency category and calculates the percentage of users for each category out of
+    the total of 33 users. The results are sorted in descending order of usage frequency, providing insights into user engagement
+    with the fitness tracker.
+*/
+
 SELECT 
     COUNT(NumberTimesUse) AS Users,
     ROUND((COUNT(NumberTimesUse) / 33) * 100, 2) AS PrecentageOfUsers,
@@ -66,6 +72,13 @@ GROUP BY NumberTimesUse
 ORDER BY NumberTimesUse DESC;
 
 -- Checking usage range
+/*
+   This SQL code categorizes users based on their engagement with the fitness tracker.
+   It calculates the usage frequency for each user, categorizes them as 'Active User,' 
+   'Moderate User,' or 'Light User' based on predefined ranges, and then counts how many 
+   users fall into each category. The results are ordered by the number of users in each 
+   category, providing insights into the distribution of user engagement levels with the tracker.
+*/
 SELECT 
     TrackerUsage, COUNT(TrackerUsage) AS Users
 FROM
@@ -96,6 +109,12 @@ FROM
     Daily_Activity;
 
 -- Calculate No. of People with Steps>10000 and <1000 (Recommended)
+/*
+   This SQL code categorizes users based on their average daily step count. It calculates the average steps for each user,
+   categorizes them as '10000 or more' or 'less than 10000' based on whether their average steps are equal to or greater
+   than 10,000, and then counts how many users fall into each category. The results provide insights into the distribution
+   of users meeting the recommended daily step count of 10,000 or more.
+*/
 SELECT 
     CompleteStepsRecommendation,
     COUNT(CompleteStepsRecommendation) AS UsersNumber
@@ -113,6 +132,13 @@ FROM
 GROUP BY CompleteStepsRecommendation;
 
 -- Calculate (Tracker Active during Sleep)
+/*
+   This SQL code calculates the percentage of users who use their tracker during sleep and those who don't.
+   It first counts the number of distinct users who have sleep data (SleepUsers) and subtracts this from the
+   total number of users (33 in this case) to calculate the number of users who don't use their tracker during
+   sleep (NonSleepUsers). It then calculates the percentages of SleepUsers and NonSleepUsers out of the total
+   user base (33 users) and rounds the percentages to two decimal places.
+*/
 SELECT 
     ROUND((SleepUsers / 33.0) * 100, 2) AS PercentageSleepUsers,
     ROUND((NonSleepUsers / 33.0) * 100, 2) AS PercentageNonSleepUsers
@@ -123,7 +149,17 @@ FROM
     FROM
         Sleep) AS SleepUse;
 	
--- Sleep Quality	
+-- Sleep Quality
+/*
+This SQL code calculates the percentage of users based on their sleep quality. It first calculates the sleep
+quality for each user based on the average total minutes asleep (TotalMinutesAsleep). Sleep quality is categorized
+as 'Poor Sleep' for users with less than 360 minutes of sleep, 'Good Sleep' for users with 360 to 479 minutes of
+sleep, and 'Excellent Sleep' for users with 480 or more minutes of sleep.
+
+The inner query calculates sleep quality for each user and groups the results by user ID. The outer query then
+calculates the percentage of users in each sleep quality category out of a total of 24 hours (assuming a full day)
+and rounds the percentages to two decimal places. The results are ordered by sleep quality categories.
+*/
 SELECT 
     SleepQuality,
     ROUND((COUNT(*) / 24.0) * 100, 2) AS PercentageSleepQuality
@@ -211,6 +247,13 @@ ON temp1.Id = temp2.Id and temp1.ActivityDate = temp2.SleepDay
 Group By temp1.Id
 
 -- Average MET per day per use and Comparison with the calories burned
+/*
+    This SQL code performs an inner join between two tables: 'MET' (aliased as 'temp1') and 'Daily_Activity' (aliased as 'temp2').
+    It retrieves distinct records based on the user ID (temp1.Id), activity date (temp1.ActivityDate), and calculates the
+    sum of METs (Metabolic Equivalent of Task) as 'sum_mets' and the corresponding calories burned (temp2.Calories). The
+    results are grouped by user ID, activity date, and calories. The OFFSET and FETCH FIRST clauses limit the result
+    set to the first 10 rows ordered by activity date.
+*/
 Select Distinct temp1.Id, temp1.ActivityDate, sum(temp1.METs) as sum_mets, temp2.Calories
 From MET as temp1
 inner join Daily_Activity as temp2
